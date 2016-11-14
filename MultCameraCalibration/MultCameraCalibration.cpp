9a3ea17c6fdf54cc0e2edc838afca30cd0353323
Hanli0 <x5489134@vip.qq.com>
2016-11-14 15:46:11 +0800

    #Version 2.1

--- MultCameraCalibration/MultCameraCalibration.cpp
+++ MultCameraCalibration/MultCameraCalibration.cpp
@@ -111,65 +111,6 @@ void outputCameraParam(void)
 		cout << "Error: can not save the extrinsic parameters\n";
 }
 
-void MultiImage_OneWin(const std::string& MultiShow_WinName, const vector<Mat>& SrcImg_V, CvSize SubPlot, CvSize ImgMax_Size)
-{
-
-	Mat Disp_Img;
-	//Width of source image  
-	CvSize Img_OrigSize = cvSize(SrcImg_V[0].cols, SrcImg_V[0].rows);
-	//******************** Set the width for displayed image ********************//  
-	//Width vs height ratio of source image  
-	float WH_Ratio_Orig = Img_OrigSize.width / (float)Img_OrigSize.height;
-	CvSize ImgDisp_Size = cvSize(100, 100);
-	if (Img_OrigSize.width > ImgMax_Size.width)
-		ImgDisp_Size = cvSize(ImgMax_Size.width, (int)ImgMax_Size.width / WH_Ratio_Orig);
-	else if (Img_OrigSize.height > ImgMax_Size.height)
-		ImgDisp_Size = cvSize((int)ImgMax_Size.height*WH_Ratio_Orig, ImgMax_Size.height);
-	else
-		ImgDisp_Size = cvSize(Img_OrigSize.width, Img_OrigSize.height);
-	//******************** Check Image numbers with Subplot layout ********************//  
-	int Img_Num = (int)SrcImg_V.size();
-	if (Img_Num > SubPlot.width * SubPlot.height)
-	{
-		cout << "Your SubPlot Setting is too small !" << endl;
-		exit(0);
-	}
-	//******************** Blank setting ********************//  
-	CvSize DispBlank_Edge = cvSize(80, 60);
-	CvSize DispBlank_Gap = cvSize(15, 15);
-	//******************** Size for Window ********************//  
-	Disp_Img.create(Size(ImgDisp_Size.width*SubPlot.width + DispBlank_Edge.width + (SubPlot.width - 1)*DispBlank_Gap.width,
-		ImgDisp_Size.height*SubPlot.height + DispBlank_Edge.height + (SubPlot.height - 1)*DispBlank_Gap.height), CV_8UC3);
-	Disp_Img.setTo(0);//Background  
-					  //Left top position for each image  
-	int EdgeBlank_X = (Disp_Img.cols - (ImgDisp_Size.width*SubPlot.width + (SubPlot.width - 1)*DispBlank_Gap.width)) / 2;
-	int EdgeBlank_Y = (Disp_Img.rows - (ImgDisp_Size.height*SubPlot.height + (SubPlot.height - 1)*DispBlank_Gap.height)) / 2;
-	CvPoint LT_BasePos = cvPoint(EdgeBlank_X, EdgeBlank_Y);
-	CvPoint LT_Pos = LT_BasePos;
-
-	//Display all images  
-	for (int i = 0; i < Img_Num; i++)
-	{
-		//Obtain the left top position  
-		if ((i%SubPlot.width == 0) && (LT_Pos.x != LT_BasePos.x))
-		{
-			LT_Pos.x = LT_BasePos.x;
-			LT_Pos.y += (DispBlank_Gap.height + ImgDisp_Size.height);
-		}
-		//Writting each to Window's Image  
-		Mat imgROI = Disp_Img(Rect(LT_Pos.x, LT_Pos.y, ImgDisp_Size.width, ImgDisp_Size.height));
-		resize(SrcImg_V[i], imgROI, Size(ImgDisp_Size.width, ImgDisp_Size.height));
-
-		LT_Pos.x += (DispBlank_Gap.width + ImgDisp_Size.width);
-	}
-	//putText(Disp_Img, "Left Capture", cv::Point(200, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
-	//putText(Disp_Img, "Right Capture", cv::Point(600, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
-	//putText(Disp_Img, "Left Chessboard", cv::Point(175, 342), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
-	//putText(Disp_Img, "Right Chessboard", cv::Point(575, 342), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
-
-	cvShowImage(MultiShow_WinName.c_str(), &(IplImage(Disp_Img)));
-}
-
 int main()
 {
     
@@ -214,6 +155,7 @@ int main()
 		//右上图像画到画布上
 		//获得画布的另一部分
 		canvasPart = canvas(Rect(w, 0, w, h));
+		
 		resize(right, canvasPart, canvasPart.size(), 0, 0, INTER_LINEAR);
 		//读取按键W
 		if (cvWaitKey(10) == 'w')
@@ -270,7 +212,12 @@ int main()
 				cout << "The image No." << goodFrameCount + 1 << " is bad please try again!" << endl;
 			}
 		}
-
+		putText(canvas, "LeftCapture", cv::Point(5, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+		putText(canvas, "RightCapture", cv::Point(325, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+		putText(canvas, "LeftChessboard", cv::Point(5, 255), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+		putText(canvas, "RightChessboard", cv::Point(325, 255), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+		putText(canvas, "LeftRectified", cv::Point(5, 495), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+		putText(canvas, "RightRectified", cv::Point(325, 495), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
 		imshow("Output", canvas);
 		//读取按键Q
 		if (waitKey(10) == 'q')
@@ -338,8 +285,11 @@ int main()
 	//右下图像画到画布上
 	canvasPart = canvas(Rect(w, h * 2, w, h));
 	resize(rectifyImageR, canvasPart, canvasPart.size(), 0, 0, INTER_LINEAR);
-
+	putText(canvas, "LeftRectified", cv::Point(5, 495), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
+	putText(canvas, "RightRectified", cv::Point(325, 495), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 255, 255));
 	imshow("Output", canvas);
+	
+	//输出矫正后图像
 	//imwrite("ImageL",rectifyImageL);
 	//imwrite("ImageR", rectifyImageR);
 
@@ -382,7 +332,7 @@ int main()
 	for (int i = 0; i < canvas_.rows;i += 16)
 		line(canvas_, Point(0, i), Point(canvas_.cols, i), Scalar(0, 200, 0), 1, 8);
 
-	imshow("rectified", canvas_);
+	imshow("Rectified", canvas_);
 
 	cout << "wait key" << endl;
 	waitKey(0);
